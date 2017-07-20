020f31aeeeda960217f8ac0b4623f454cae537b2
Billy <mr.bill.ly@gmail.com>
2017-07-20 11:58:19 -0700

    bottom navi
    created bottom navi bar
    displaying correct icons and listener working

--- SousBoxv2/app/src/main/java/billy1301/sousboxv2/MainActivity.java
+++ SousBoxv2/app/src/main/java/billy1301/sousboxv2/MainActivity.java
@@ -1,13 +1,71 @@
 package billy1301.sousboxv2;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        disableShiftMode(bottomNavigationView);
+        setBottomNavigationView();
     }
+
+
+    private void setBottomNavigationView() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_favorites:
+                        Toast.makeText(MainActivity.this, "favorite button pressed", Toast.LENGTH_SHORT).show();
+                    case R.id.action_lists:
+
+                    case R.id.action_random:
+
+                    case R.id.action_settings:
+
+                }
+                return true;
+            }
+        });
+    }
+
+
+    public void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                    //noinspection RestrictedApi
+                item.setShiftingMode(false);
+                    // set once again checked value, so view will be updated
+                    //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BNVHelper", "Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e);
+        }
+    }
+
 }
